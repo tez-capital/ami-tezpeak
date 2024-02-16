077820955689ad9078a21dbc7224fed55d585e39
TezpeakReleaseDispatch <tezpeak.release.dispatch@alis.is>
2024-02-16 18:31:34 +0000

    updated-tezpeak-version

--- src/__tezpeak/constants.lua
+++ src/__tezpeak/constants.lua
@@ -3,7 +3,7 @@ return {
         "config.hjson"
     },
     sources = {
-        ["linux-x86_x64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.1.0/tezpeak-linux-amd64",
-        ["linux-arm64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.1.0/tezpeak-linux-arm64"
+        ["linux-x86_x64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.1.1/tezpeak-linux-amd64",
+        ["linux-arm64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.1.1/tezpeak-linux-arm64"
     }
 }
