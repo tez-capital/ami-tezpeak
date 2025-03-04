1baf9e4eaaf6968e6e982e91efb99a5fa2dc75cc
TezpeakReleaseDispatch <tezpeak.release.dispatch@alis.is>
2025-03-04 21:49:07 +0000

    updated-tezpeak-version

--- src/__tezpeak/constants.lua
+++ src/__tezpeak/constants.lua
@@ -3,8 +3,8 @@ return {
         "config.hjson"
     },
     sources = {
-        ["linux-x86_64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.6.0-beta/tezpeak-linux-amd64",
-        ["linux-arm64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.6.0-beta/tezpeak-linux-arm64",
+        ["linux-x86_64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.6.1-beta/tezpeak-linux-amd64",
+        ["linux-arm64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.6.1-beta/tezpeak-linux-arm64",
         ["arc-linux-x86_64"] = "https://github.com/alis-is/arc-releases/releases/download/0.0.11/arc-x86_64-unknown-linux-musl",
         ["arc-linux-arm64"] = "https://github.com/alis-is/arc-releases/releases/download/0.0.11/arc-aarch64-unknown-linux-musl",
     }
