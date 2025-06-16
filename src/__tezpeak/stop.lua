83782bfc00ab47fe6914954462ab2b3823dfbede
v <cryi@tutanota.com>
2025-06-16 23:14:22 +0200

    fix services reference

--- src/__tezpeak/stop.lua
+++ src/__tezpeak/stop.lua
@@ -1,5 +1,5 @@
 local service_manager = require"__xtz.service-manager"
-local services = require"__xtz.services"
+local services = require"__tezpeak.services"
 
 log_info("stopping tezpeak services... this may take few minutes.")
 
