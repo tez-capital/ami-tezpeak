d32e2fdf45742437d08930fdad548a45891ddcaf
v <cryi@tutanota.com>
2025-08-18 00:14:24 +0200

    updated arc

--- src/__tezpeak/constants.lua
+++ src/__tezpeak/constants.lua
@@ -6,7 +6,7 @@ return {
         ["linux-x86_64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.7.0-beta/tezpeak-linux-amd64",
         ["linux-arm64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.7.0-beta/tezpeak-linux-arm64",
         ["macos-arm64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.7.0-beta/tezpeak-macos-arm64",
-        ["arc-linux-x86_64"] = "https://github.com/alis-is/arc-releases/releases/download/0.0.11/arc-x86_64-unknown-linux-musl",
-        ["arc-linux-arm64"] = "https://github.com/alis-is/arc-releases/releases/download/0.0.11/arc-aarch64-unknown-linux-musl",
+        ["arc-linux-x86_64"] = "https://github.com/alis-is/arc-releases/releases/download/0.0.12/arc-x86_64-unknown-linux-musl",
+        ["arc-linux-arm64"] = "https://github.com/alis-is/arc-releases/releases/download/0.0.12/arc-aarch64-unknown-linux-musl",
     }
 }
