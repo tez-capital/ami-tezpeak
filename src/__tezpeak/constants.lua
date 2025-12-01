c7011cfb0526f55ed22b0d8a330f1b576cc88910
TezpeakReleaseDispatch <tezpeak.release.dispatch@alis.is>
2025-12-01 21:28:54 +0000

    updated-tezpeak-version

--- src/__tezpeak/constants.lua
+++ src/__tezpeak/constants.lua
@@ -3,9 +3,9 @@ return {
         "config.hjson"
     },
     sources = {
-        ["linux-x86_64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.8.1-beta/tezpeak-linux-amd64",
-        ["linux-arm64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.8.1-beta/tezpeak-linux-arm64",
-        ["macos-arm64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.8.1-beta/tezpeak-macos-arm64",
+        ["linux-x86_64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.8.2-beta/tezpeak-linux-amd64",
+        ["linux-arm64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.8.2-beta/tezpeak-linux-arm64",
+        ["macos-arm64"] = "https://github.com/tez-capital/tezpeak/releases/download/0.8.2-beta/tezpeak-macos-arm64",
         ["arc-linux-x86_64"] = "https://github.com/alis-is/arc-releases/releases/download/0.0.12/arc-x86_64-unknown-linux-musl",
         ["arc-linux-arm64"] = "https://github.com/alis-is/arc-releases/releases/download/0.0.12/arc-aarch64-unknown-linux-musl",
     }
